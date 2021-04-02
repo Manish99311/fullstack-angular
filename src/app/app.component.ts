f9fe6dc5209857f644d5a0180c73c4d05eed5f16
Manish99311 <52909385+Manish99311@users.noreply.github.com>
2021-04-02 18:59:43 +0530

    created the banner and added logic to navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'common-ui';
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e) {
+    this.onScroll();
+  }
+
+  public onScroll() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
+import { HeaderBannerComponent } from './component/header-banner/header-banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HeaderBannerComponent
   ],
   imports: [
     BrowserModule,
